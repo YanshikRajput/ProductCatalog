4f5f167df6e97f0cd64484c37842239cf83ea296
YanshikRajput <75445620+YanshikRajput@users.noreply.github.com>
2021-04-19 11:33:33 +0530

    Assignment-2
    ProductCatalog

---
+++ ProductCatalog/ProductCatalog/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCatalog.Entities
+{
+      public class Product
+      {
+        public static int ID = 1;
+        public int Product_ID { get; set; }
+        public static int GenerateProductId()
+        {
+            return ID++;
+        }
+        public string product_Name { get; set; }
+        public string Manufacturer { get; set; }
+        public string ProductShortCode { get; set; }
+        public List<Category> ProductCategory { get; set; }
+        public Product()
+        {
+            this.ProductCategory = new List<Category>();
+        }
+        public string ProductDescription { get; set; }
+        public int Selling_Price { get; set; }
+    }
+}
---
+++ ProductCatalog/ProductCatalog/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ProductCatalog.Entities;
+
+namespace ProductCatalog
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Product-Catalog");
+            Console.WriteLine("1.PRODUCT");
+            Console.WriteLine("2.CATEGORY");
+            Console.WriteLine("3.EXIT-APP");
+
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            switch (ch)
+            {
+                case '1':
+                    ProductOperations.ProductOperationMenu();
+                    break;
+                case '2':
+                    CategoryOperations.CategoryOperationMenu();
+                    break;
+                case '3':
+                    break;
+                default:
+                    Console.WriteLine("Please Enter Valid Options");
+                    break;
+            }
+        }
+
+    }
+}
